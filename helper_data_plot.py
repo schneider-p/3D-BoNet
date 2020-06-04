bb5168174cdf55e0169962ab9c650be01bbbc076
Pascasl Schneider <pschneider@uni-koblenz.de>
2020-06-04 07:16:41 +0200

    Update visualization to work with open3d 0.10.0

--- helper_data_plot.py
+++ helper_data_plot.py
@@ -4,7 +4,7 @@ import scipy.io
 import copy
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-from open3d import linux as open3d  ## pip install open3d-python==0.3.0
+import open3d as o3d
 import random
 import colorsys
 
@@ -21,16 +21,16 @@ class Plot:
 
 	@staticmethod
 	def draw_pc(pc_xyzrgb):
-		pc = open3d.PointCloud()
-		pc.points = open3d.Vector3dVector(pc_xyzrgb[:, 0:3])
+		pc = o3d.geometry.PointCloud()
+		pc.points = o3d.utility.Vector3dVector(pc_xyzrgb[:, 0:3])
 		if pc_xyzrgb.shape[1]==3:
-			open3d.draw_geometries([pc])
+			o3d.visualization.draw_geometries([pc])
 			return 0
 		if np.max(pc_xyzrgb[:, 3:6])>20: ## 0-255
-			pc.colors = open3d.Vector3dVector(pc_xyzrgb[:, 3:6]/255.)
+			pc.colors = o3d.utility.Vector3dVector(pc_xyzrgb[:, 3:6]/255.)
 		else:
-			pc.colors = open3d.Vector3dVector(pc_xyzrgb[:, 3:6])
-		open3d.draw_geometries([pc])
+			pc.colors = o3d.utility.Vector3dVector(pc_xyzrgb[:, 3:6])
+		o3d.visualization.draw_geometries([pc])
 		return 0
 
 	@staticmethod
--- main_eval.py
+++ main_eval.py
@@ -210,12 +210,12 @@ class Evaluation:
 			ins_pred_all = volume[tuple(pc_xyz_int.T)]
 
 			#### if you need to visulize, please uncomment the follow lines
-			#from helper_data_plot import Plot as Plot
-			#Plot.draw_pc(np.concatenate([pc_all[:,9:12], pc_all[:,3:6]], axis=1))
-			#Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=ins_gt_all)
-			#Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=ins_pred_all)
-			#Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=sem_gt_all)
-			#Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=sem_pred_all)
+			from helper_data_plot import Plot as Plot
+			Plot.draw_pc(np.concatenate([pc_all[:,9:12], pc_all[:,3:6]], axis=1))
+			Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=ins_gt_all)
+			Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=ins_pred_all)
+			Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=sem_gt_all)
+			Plot.draw_pc_semins(pc_xyz=pc_all[:, 9:12], pc_semins=sem_pred_all)
 			####
 
 			###################
